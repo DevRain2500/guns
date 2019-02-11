603c3ee3e92ab89fcfb0e85a0d58b3da75062665
Shredxt <47064656+shredxt@users.noreply.github.com>
2019-02-11 23:12:02 +0000

    Add files via upload

--- client.lua
+++ client.lua
@@ -4,34 +4,37 @@ local playerPed = GetPlayerPed(-1)
 
 --Get guns via text commands
 RegisterCommand("sg", function()
-    if sgCheckedOut then
+    if not sgCheckedOut then
         if exports.GTALife:countItems("Police Key Card") >= 1 then
             giveWeapon("weapon_pumpshotgun")
             GiveWeaponComponentToPed(playerPed, GetHashKey("weapon_pumpshotgun"), GetHashKey("COMPONENT_AT_AR_FLSH"))
             TriggerEvent("ShowInformationLeft", 2000, "You grabbed your Shotgun...")
             --notify('~g~Received Shotgun.')
+            sgCheckedOut = true
         end
     else
         removeWeapon("weapon_pumpshotgun")
         TriggerEvent("ShowInformationLeft", 2000, "You stowed your Shotgun...")
         --notify('~r~Removed shotgun.')
+        sgCheckedOut = false
     end
 end)
 
 RegisterCommand("ar", function()
-    if arCheckedOut then
+    if not arCheckedOut then
         if exports.GTALife:countItems("Police Key Card") >= 1 then
             giveWeapon("weapon_carbinerifle")
             GiveWeaponComponentToPed(playerPed, GetHashKey("weapon_carbinerifle"), GetHashKey("COMPONENT_AT_AR_FLSH"))
             GiveWeaponComponentToPed(playerPed, GetHashKey("weapon_carbinerifle"), GetHashKey("COMPONENT_AT_SCOPE_MEDIUM"))
             TriggerEvent("ShowInformationLeft", 2000, "You grabbed your Carbine Rifle...")
             --notify('~g~Received Carbine Rifle.')
+            arCheckedOut = true
         end
-        else
-            removeWeapon("weapon_carbinerifle")
-            TriggerEvent("ShowInformationLeft", 2000, "You stowed Carbine Rifle...")
-            --notify('~r~Removed Carbine Rifle.')
-        end
+    else
+        removeWeapon("weapon_carbinerifle")
+        TriggerEvent("ShowInformationLeft", 2000, "You stowed Carbine Rifle...")
+        --notify('~r~Removed Carbine Rifle.')
+        arCheckedOut = false
     end
 end)
 
@@ -41,5 +44,7 @@ RegisterCommand("remove", function()
         removeWeapon("weapon_carbinerifle")
         TriggerEvent("ShowInformationLeft", 2000, "You stowed the kitchen sink...")
         notify('~r~Removed Shotgun & Carbine Rifle.')
+        sgCheckedOut = false
+        arCheckedOut = false
     end
 end)
